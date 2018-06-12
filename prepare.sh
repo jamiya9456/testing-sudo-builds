bc0df28f3149dcadebdfe88e7c3296003563c592
nicholos649 <nicholos649@isbjct4e.com>
2018-06-12 10:51:03 -0400

    test

--- prepare.sh
+++ prepare.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
 # called by Travis CI
-
-php -dphar.readonly=0 ./utils/make-phar.php easyengine.phar --quite  > /dev/null
-php easyengine.phar cli info
+version=$(head -n 1 VERSION)
+version="$(echo $version | xargs)"
+version+="-nightly"
+echo $version > VERSION
+cat VERSION
+#php -dphar.readonly=0 ./utils/make-phar.php easyengine.phar --quite  > /dev/null
+#php easyengine.phar cli info
