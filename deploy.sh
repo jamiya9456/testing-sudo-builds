ccc8ab0fe9d0215c9fc2321e857599a4b9507dee
nicholos649 <nicholos649@isbjct4e.com>
2018-06-13 03:38:01 -0400

    test

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,5 @@
 sudo /usr/bin/php7.2 easyengine.phar site list
 wget -qO ee rt.cx/ee4beta && sudo bash ee
 sudo ee cli info
+sudo ee site list
+sudo ee site create test.test
--- prepare.sh
+++ prepare.sh
@@ -1,20 +1,4 @@
 #!/bin/bash
-function setup_php_modules {
-    php_modules=( pcntl curl sqlite3 )
-    if command -v php > /dev/null 2>&1; then
-      default_php_version="$(readlink -f /usr/bin/php | gawk -F "php" '{ print $2}')"
-      echo "Installed PHP : $default_php_version"
-      echo "Checking if required PHP modules are installed..."
-      for module in "${php_modules[@]}"; do
-        if ! php -m | grep $module > /dev/null 2>&1; then
-          echo "$module not installed. Installing..."
-          apt install -y php$default_php_version-$module
-        else
-          echo "$module is already installed"
-        fi
-      done
-    fi
-}
-setup_php_modules
+sudo apt install php7.2-curl php7.2-sqlite3 -y
 sudo /usr/bin/php7.2 -dphar.readonly=0 ./utils/make-phar.php easyengine.phar --quite  > /dev/null
 sudo /usr/bin/php7.2 easyengine.phar cli info
