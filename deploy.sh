380547df37eff7bf1a9fc75b8283f480c9c5b579
nicholos649 <nicholos649@isbjct4e.com>
2018-06-13 07:20:14 -0400

    test

--- deploy.sh
+++ deploy.sh
@@ -2,6 +2,8 @@ sudo /usr/bin/php7.2 easyengine.phar site list
 wget -qO ee rt.cx/ee4beta && sudo bash ee
 sudo ee cli info
 sudo ee site list
-sudo ee site create test.test --debug
+sudo ee site create test.test --debug --dbpass='KsPpAJSeIAJ4'
+cd ~/ee-sites/test.test
+docker-compose exec --user='www-data' php wp config create --dbuser='wordpress' --dbname='test_test' --dbpass='KsPpAJSeIAJ4' --dbhost='db'  --dbcharset=utf8
 curl -XGET -IL test.test
 curl test.test
