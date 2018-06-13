21235ccc9d733a31f7d61665d4ff7dde874cc2f4
nicholos649 <nicholos649@isbjct4e.com>
2018-06-13 05:12:46 -0400

    test

--- deploy.sh
+++ deploy.sh
@@ -2,6 +2,6 @@ sudo /usr/bin/php7.2 easyengine.phar site list
 wget -qO ee rt.cx/ee4beta && sudo bash ee
 sudo ee cli info
 sudo ee site list
-sudo ee site create test.test
+sudo ee site create test.test --debug
 curl -XGET -IL test.test
 curl test.test
