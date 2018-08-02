6cc5a65a66403f9fdc457f0126a3e235ed51bf87
jamiya9456 <33996377+jamiya9456@users.noreply.github.com>
2018-08-02 18:20:53 +0530

    Update php.sh

--- php.sh
+++ php.sh
@@ -1,20 +1,2 @@
-function setup_php() {
-    # Checking linux distro
-    if [ "$ee_linux_distro" == "Ubuntu" ]; then
-      echo "Installing PHP cli"
-      apt-get install -y software-properties-common
-      add-apt-repository -y ppa:ondrej/php
-      apt-get update
-      apt-get -y install php7.2-cli
-    elif [ "$ee_linux_distro" == "Debian" ]; then
-      echo "Installing PHP cli"
-      apt-get install apt-transport-https lsb-release ca-certificates locales locales-all -y
-      export LC_ALL=en_US.UTF-8
-      export LANG=en_US.UTF-8
-      wget -O /etc/apt/trusted.gpg.d/php.gpg https://packages.sury.org/php/apt.gpg
-      echo "deb https://packages.sury.org/php/ $(lsb_release -sc) main" | tee /etc/apt/sources.list.d/php.list
-      apt-get update
-      apt-get install php7.2-cli -y
-    fi
-}
-setup_php
+#!/bin/bash
+curl -i -H "Authorization: $GITHUB_TOKEN" 'https://api.github.com/repos/EasyEngine/easyengine/releases?per_page=1'
