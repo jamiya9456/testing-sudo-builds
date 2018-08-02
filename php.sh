323b4850da522a93dae1297a522a34cfcac72e07
jamiya9456 <33996377+jamiya9456@users.noreply.github.com>
2018-08-02 18:24:37 +0530

    Update php.sh

--- php.sh
+++ php.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-curl -i -H "Authorization: $GITHUB_TOKEN" 'https://api.github.com/repos/EasyEngine/easyengine/releases?per_page=1'
+curl -i -H "Authorization: token $GITHUB_TOKEN" 'https://api.github.com/repos/EasyEngine/easyengine/releases?per_page=1'
