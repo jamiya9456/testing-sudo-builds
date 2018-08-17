62a09a1854735aec23eedec80df901baa0465172
jamiya9456 <33996377+jamiya9456@users.noreply.github.com>
2018-08-17 17:18:14 +0530

    Update deploy.sh
    [BREAKING CHANGES]

--- deploy.sh
+++ deploy.sh
@@ -1,11 +1,12 @@
-git --no-pager log --graph
-echo "Commit msg: $TRAVIS_COMMIT_MESSAGE"
+
 
 if [[ "false" != "$TRAVIS_PULL_REQUEST" ]]; then
   commit_msg="$(git --no-pager log HEAD~1...HEAD~2 --pretty=%B)"
 else
   commit_msg=$TRAVIS_COMMIT_MESSAGE
 fi
+
+echo "Commit msg: $commit_msg"
 
 if [[ $commit_msg = *"[BREAKING CHANGES]"* ]]; then
   echo "It's there!"
