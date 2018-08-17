f7430d5cc16055fa9998ebd093d77644d7240e70
jamiya9456 <33996377+jamiya9456@users.noreply.github.com>
2018-08-17 12:54:38 +0530

    [BREAKING CHANGES] Add db changes to Core

--- deploy.sh
+++ deploy.sh
@@ -1,9 +1,3 @@
-sudo /usr/bin/php7.2 easyengine.phar site list
-wget -qO ee rt.cx/ee4beta && sudo bash ee
-sudo ee cli info
-sudo ee site list
-sudo ee site create test.test --debug --dbpass='KsPpAJSeIAJ4'
-cd ~/ee-sites/test.test
-docker-compose exec --user='www-data' php wp config create --dbuser='wordpress' --dbname='test_test' --dbpass='KsPpAJSeIAJ4' --dbhost='db'  --dbcharset=utf8
-curl -XGET -IL test.test
-curl test.test
+if [[ $TRAVIS_COMMIT_MESSAGE = *"[BREAKING CHANGES]"* ]]; then
+  echo "It's there!"
+fi
