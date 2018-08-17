15eeaa3bbfe4b7bb1b48288481c9793adc400323
jamiya9456 <33996377+jamiya9456@users.noreply.github.com>
2018-08-17 17:46:13 +0530

    two
    [BREAKING CHANGES]
    REPO=kirtangajjar/site-wp-command:develop

--- deploy.sh
+++ deploy.sh
@@ -1,12 +1,43 @@
 if [[ "false" != "$TRAVIS_PULL_REQUEST" ]]; then
-  commit_msg="$(git --no-pager log -2 --pretty=%B)"
+	commit_msg="$(git --no-pager log -2 --pretty=%B)"
 else
-  commit_msg=$TRAVIS_COMMIT_MESSAGE
+	commit_msg=$TRAVIS_COMMIT_MESSAGE
 fi
 
 echo "Commit msg: $commit_msg"
 
-if [[ $commit_msg = *"[BREAKING CHANGES]"* ]]; then
-  echo "It's there!"
-fi
+echo "Starting test suite for other commands"
 
+if [[ $commit_msg = *"[BREAKING CHANGES]"* ]]; then
+
+	echo "[BREAKING CHANGES] found."
+	pattern='\<REPO\>.*\b'
+
+	declare -a repos
+
+	for word in $commit_message; do
+		[[ $word =~ $pattern ]]
+		if [[ ${BASH_REMATCH[0]} ]]; then
+			repos+=(${BASH_REMATCH[0]})
+		fi
+	done
+
+	pushd vendor/easyengine
+
+	for repo in ${repos[@]}; do
+		owner_name_unclean=${repo%%\/*}
+		owner_name=${owner_name_unclean#*\=}
+		repo_with_branch=${repo#*\/}
+		repo_name=${repo_with_branch%\:*}
+		repo_branch=${repo_with_branch#*\:}
+		sudo rm -r "$repo_name"
+		git clone https://github.com/"$owner_name"/"$repo_name".git -b "$repo_branch"
+		pushd "$repo_name"
+		echo "Updated to easyengine/$repo_name to $owner_name/$repo_name"
+		git remote -v
+		git branch
+		popd
+	done
+
+	popd
+fi
